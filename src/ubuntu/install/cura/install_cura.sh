03d0ca3efa0a704084c9c53230deb5f44c705a11
Keshav <keshav.deeljur@gmail.com>
2024-01-16 00:06:48 +0000

    updated bg and deleted unneeded custom workspaces

--- src/ubuntu/install/cura/install_cura.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-apt-get update
-apt-get install wget -y
-#wget https://github.com/Ultimaker/Cura/releases/download/5.5.0/UltiMaker-Cura-5.5.0-linux-X64.AppImage -O Cura.AppImage
-wget https://github.com/Ultimaker/Cura/releases/download/5.4.0/UltiMaker-Cura-5.4.0-linux.AppImage -O  Cura.AppImage
-chmod +x Cura.AppImage
-./Cura.AppImage --appimage-extract
-rm Cura.AppImage
--- src/ubuntu/install/kicad/install_kicad.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-apt-get update
-add-apt-repository -y ppa:kicad/kicad-7.0-releases
-apt-get install -y kicad
-apt-get autoremove -y
